3751cbdf31206b6e999b36fb3580a865c9913f01
Anmol Jawandha <anmoljawandha6@gmail.com>
2017-01-27 03:04:33 -0800

    sql

--- letstalk/schema.sql
+++ letstalk/schema.sql
@@ -1,6 +1,38 @@
-drop table if exists entries;
-create table entries (
-  id integer primary key autoincrement,
-  title text not null,
-  'text' text not null
+CREATE DATABASE lData;
+ 
+CREATE TABLE User(
+userId INT NOT NULL AUTO_INCREMENT,
+userName VARCHAR(100) NOT NULL,
+password VARCHAR(40) NOT NULL,
+PRIMARY KEY(userId)
+ );
+
+USE `lData`$$
+CREATE PROCEDURE `spCreateUser` (
+IN p_Username varchar(50),
+IN p_Password varchar(50)
+)
+BEGIN
+
+if ( select exists (select 1 from User where Username = p_username) ) THEN
+
+    select 'Username Exists !!';
+
+ELSE
+
+insert into tblUser
+(
+    Username,
+    Password
+)
+values
+(
+    p_Username,
+    p_Password
 );
+
+END IF;
+
+END$$
+
+DELIMITER ;
