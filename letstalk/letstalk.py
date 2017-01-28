9931b3c1ff2cea2d0b9e8016638c98a317057f35
Anmol Jawandha <anmoljawandha6@gmail.com>
2017-01-27 17:22:47 -0800

    new account sql

--- letstalk/letstalk.py
+++ letstalk/letstalk.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 #specify app configurations for database 
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
-app.config['MYSQL_DATABASE_DB'] = 'blogs'
+app.config['MYSQL_DATABASE_DB'] = 'lData'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 #intiialize mysql database object
@@ -37,8 +37,17 @@ def showSignin():
 
 def createNewAccount():
 	try:
+		name = request.form('inputName')
 		username = request.form('inputEmail')
 		password = request.form('inputPassword')
+		cursor.callproc('spCreateUser',(_userEmail,_userPassword))
+		data = cursor.fetchall()
+	if len(data) is 0:
+   		conn.commit()
+   		return {'StatusCode':'200','Message': 'User creation success'}
+	else:
+		return {'StatusCode':'1000','Message': str(data[0])}
+
 
 	except Exception as e:
 		return "Username taken, please try again!"
@@ -58,6 +67,10 @@ def Authenticate():
 		cursor = mysql.connect().cursor() #object to handle database communication
 		cursor.execute("SELECT * from User where Username='" + username + "' and Password='" + password + "'")
 		data = cursor.fetchone()
+		if data is None:
+			return "Invalid Username/Password"
+		else:
+			return "login succcesful!"
 
 	except Exception as e:
 		return "User does not exist - please try again!"
